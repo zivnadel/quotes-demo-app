91aba9ba35ce5c3c2a624ce9a8d98eab709e7100
zivnadel <zywn1414@gmail.com>
2022-06-10 16:28:25 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Suspense } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
+const AddQuote = React.lazy(() => import("./pages/AddQuote"));
+const QuoteDetails = React.lazy(() => import("./pages/QuoteDetails"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+
+const App = () => {
+  return (
+    <Layout>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetails />
+          </Route>
+          <Route path="/add-quote">
+            <AddQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Layout>
+  );
+};
+
+export default App;
